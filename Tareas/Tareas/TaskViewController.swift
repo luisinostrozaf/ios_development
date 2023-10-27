85609ad49b61bf0d1e4587f7ce0b5d7eee447cac
Luis Inostroza <50220590+luisinostrozaf@users.noreply.github.com>
2023-10-27 17:13:20 -0300

    First App (in construction)

---
+++ Tareas/Tareas/TaskViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TaskViewController.swift
+//  Tareas
+//
+//  Created by Luis Inostroza Flores on 26-10-23.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
